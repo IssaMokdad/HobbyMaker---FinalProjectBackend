eeea39cb371781bfe21d51dadf5df8bb4ea62e14
IssaMokdad <issa.almukdad@gmail.com>
2020-04-29 21:54:37 +0300

    Create new laravel project, installing laravel passport package, installing Search Results create login, logout, signup, password reset and user routes, installing fruitcake/laravel-cors, create default avatars for users by installing laravolt library, add notifications for password reset and account activation, add lang to response messages

---
+++ app/Http/Controllers/Auth/AuthController.php
@@ -0,0 +1,172 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Carbon\Carbon;
+use Storage;
+use Avatar;
+use App\Notifications\SignupActivate;
+use App\Notifications\SignupActivated;
+use App\User;
+use Validator;
+use Str;
+
+class AuthController extends Controller
+{
+    /**
+     * Create user deactivate and send notification to activate account user
+     *
+     * @param  [string] name
+     * @param  [string] email
+     * @param  [string] password
+     * @param  [string] password_confirmation
+     * @return [string] message
+     */
+    public function signup(Request $request)
+    {
+
+        $validator  =   Validator::make($request->all(),
+        [
+            'first_name' => 'required|string',
+            'last_name' => 'required|string',
+            'gender' => 'required|in:male,female',
+            'email' => 'required|string|email|unique:users',
+            'birthday' => 'required|date',
+            'password' => 'required|string|min:8'
+        ]
+    );
+
+    if($validator->fails()) {
+        return response()->json(['Validation errors' => $validator->errors()]);
+    }
+
+
+        $user = new User([
+            'first_name' => $request->first_name,
+            'last_name' => $request->last_name,
+            'email' => $request->email,
+            'gender' => $request->gender,
+            'birthday'=>$request->birthday,
+            'password' => bcrypt($request->password),
+            'activation_token' => Str::random(60)
+        ]);
+
+        $user->save();
+
+        $avatar = Avatar::create($user->first_name)->getImageObject()->encode('png');
+        Storage::put('avatars/'.$user->id.'/avatar.png', (string) $avatar);
+
+            $user->notify(new SignupActivate($user));
+
+        return response()->json([
+            'message' => __('auth.signup_success')
+        ], 201);
+    }
+
+    /**
+     * Confirm your account user (Activate)
+     *
+     * @param  [type] $token
+     * @return [string] message
+     * @return [obj] user
+     */
+    public function signupActivate($token)
+    {
+
+        $user = User::where('activation_token', $token)->first();
+
+        if (!$user) {
+            return response()->json([
+                'message' => __('auth.token_invalid')
+            ], 404);
+        }
+
+        $user->active = true;
+        $user->activation_token = '';
+        $user->save();
+
+        $user->notify(new SignupActivated($user));
+
+        return $user;
+    }
+
+    /**
+     * Login user and create token
+     *
+     * @param  [string] email
+     * @param  [string] password
+     * @param  [boolean] remember_me
+     * @return [string] access_token
+     * @return [string] token_type
+     * @return [string] expires_at
+     */
+    public function login(Request $request)
+    {
+
+        $validator  =   Validator::make($request->all(),
+        [
+            'email' => 'required|string|email',
+            'password' => 'required|string',
+            // 'remember_me' => 'boolean'
+        ]
+    );
+
+    if($validator->fails()) {
+        return response()->json(['Validation errors' => $validator->errors()]);
+    }
+
+
+        $credentials = request(['email', 'password']);
+        $credentials['active'] = 1;
+        $credentials['deleted_at'] = null;
+
+        if(!Auth::attempt($credentials))
+            return response()->json([
+                'message' => __('auth.login_failed')
+            ], 401);
+
+        $user = $request->user();
+
+        $tokenResult = $user->createToken('Personal Access Token');
+        $token = $tokenResult->token;
+
+        // if ($request->remember_me)
+        //     $token->expires_at = Carbon::now()->addWeeks(1);
+
+        $token->save();
+
+        return response()->json([
+            'message'      => __('auth.login_success'),
+            'access_token' => $tokenResult->accessToken,
+            'token_type' => 'Bearer',
+            'expires_at' => Carbon::parse($tokenResult->token->expires_at)->toDateTimeString()
+        ]);
+    }
+
+    /**
+     * Logout user (Revoke the token)
+     *
+     * @return [string] message
+     */
+    public function logout(Request $request)
+    {
+        $request->user()->token()->revoke();
+
+        return response()->json([
+            'message' => __('auth.logout_success')
+        ]);
+    }
+
+    /**
+     * Get the authenticated User
+     *
+     * @return [json] user object
+     */
+    public function user(Request $request)
+    {
+        return response()->json($request->user());
+    }
+}
---
+++ app/User.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App;
+
+use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Laravel\Passport\HasApiTokens;
+use Illuminate\Database\Eloquent\SoftDeletes;
+class User extends Authenticatable
+{
+    use Notifiable, HasApiTokens, SoftDeletes;
+
+    protected $dates = ['deleted_at'];
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'first_name', 'last_name','active', 'gender','birthday', 'email', 'password','activation_token', 'avatar'
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password', 'remember_token', 'activation_token'
+    ];
+
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+    ];
+}
